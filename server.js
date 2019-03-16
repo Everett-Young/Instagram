fd7b4d67a24d02cc02997647ad21212345edbf3a
Everett-Young <krasovskiyp@gmail.com>
2019-03-16 12:18:12 +0200

    fire serv

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 /Install express server
 const express = require('express');
 const app = express();
-
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
